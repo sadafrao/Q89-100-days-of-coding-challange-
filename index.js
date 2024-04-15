4a5c990907acce73f4d14455444c69dc73d0282a
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:30:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Q89
+//this function convert string into num
+function convertstring(str) {
+    return parseFloat(str); // convert string in number
+}
+console.log(convertstring("124379"));
+console.log(convertstring("1989"));
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Q89
+//this function convert string into num
+function convertstring(str : string): number{
+    return parseFloat(str); // convert string in number
+}
+console.log(convertstring("124379"));
+console.log(convertstring("1989"));
